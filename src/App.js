174cbdf551cb97774cf8f51aa4ae5df82bcc0283
Abhishek Ranjan <ab85hishek@gmail.com>
2020-05-30 22:27:11 +0530

    adding components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
+import Signin from './Components/Signin/Signin';
+import Register from './Components/Register/Register';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
@@ -30,7 +32,27 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+            }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }
+    })
   }
 
 
@@ -48,8 +70,9 @@ calculateFaceLocation = (data) => {
   }
 }
 
+
+
 displayFaceBox = (box) => {
-  console.log(box);
   this.setState({box : box});
 }
 
@@ -63,27 +86,64 @@ displayFaceBox = (box) => {
     .predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(response=> this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response=> {
+        if(response){
+          fetch('http://localhost:4000/image' ,{
+           method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+          id: this.state.user.id
+          })
+          })
+
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
 
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if(route=== 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
 
   render() {
+   const {isSignedIn, imageUrl, route, box} = this.state;
   return (
     <div className="App">
       <Particles className='particles' 
               params={particlesOptions}
             />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm  
-      onInputChange={this.onInputChange} 
-      onButtonSubmit={this.onButtonSubmit}  
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+      {route === 'home' 
+      ? <div>
+       <Logo />
+        <Rank 
+            name={this.state.user.name}
+            entries={this.state.user.entries}
+        />
+        <ImageLinkForm  
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}  
       />
-      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} /> 
-
+      <FaceRecognition box={box} imageUrl={imageUrl} /> 
+      </div>
+      :(
+        route === 'signin' 
+        ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+      )
+      
+      }
     </div>
     );
   }
@@ -91,3 +151,5 @@ displayFaceBox = (box) => {
 
 
 export default App;
+
+
--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
     return(
         <div>
-            <div className='white f1'>
-                {'Abhishek, your current rank is...'}
+           <div className='white f3'>
+                {`${name} , your current entry count is...`}
             </div>
             <div className='white f1'>
-                {'#1'}
+                {entries}
             </div>
-         
         </div>
     );
 }
