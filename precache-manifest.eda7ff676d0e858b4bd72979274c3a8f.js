b47d695db20112318860ce7375e8d3185ecc9ac0
Abhishek Ranjan <ab85hishek@gmail.com>
2020-06-01 22:09:17 +0530

    Updates

---
+++ precache-manifest.eda7ff676d0e858b4bd72979274c3a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970d01c944373577e7e487fe5c19c99e",
+    "url": "/face_recognition_brain/index.html"
+  },
+  {
+    "revision": "0b8b502495e01ee61cdf",
+    "url": "/face_recognition_brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9346bd0874eaaff8b13b",
+    "url": "/face_recognition_brain/static/css/main.532d869a.chunk.css"
+  },
+  {
+    "revision": "0b8b502495e01ee61cdf",
+    "url": "/face_recognition_brain/static/js/2.4b01faf4.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face_recognition_brain/static/js/2.4b01faf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9346bd0874eaaff8b13b",
+    "url": "/face_recognition_brain/static/js/main.cf2e2ea2.chunk.js"
+  },
+  {
+    "revision": "2c65a309052b3541820d",
+    "url": "/face_recognition_brain/static/js/runtime-main.fbc0e9ab.js"
+  }
+]);
